01fdda9a579b214760eff425c39dde615f6c21ae
Tim <81175852+wharvex@users.noreply.github.com>
2025-02-08 14:14:22 -0500

    first commit

---
+++ src/test/java/com/example/accessingdatajpa/AccessingdatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingdatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
