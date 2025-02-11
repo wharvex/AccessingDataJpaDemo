33e6fb5841579277053e75987e94bb87cfebc278
Tim <81175852+wharvex@users.noreply.github.com>
2025-02-11 16:07:48 -0500

    populate db if empty
    In the demo bean, try to get all the data first, and populate the db if you
    get nothing.
    
    Change datasource url to something more unique to this application.

--- src/main/java/com/example/accessingdatajpa/AccessingdatajpaApplication.java
+++ src/main/java/com/example/accessingdatajpa/AccessingdatajpaApplication.java
@@ -1,5 +1,7 @@
 package com.example.accessingdatajpa;
 
+import java.util.ArrayList;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -18,33 +20,43 @@ public class AccessingdatajpaApplication {
     @Bean
     public CommandLineRunner demo(CustomerRepository repository) {
 	return (args) -> {
-	    // save a few customers
-//	    repository.save(new Customer("Jack", "Bauer"));
-//	    repository.save(new Customer("Chloe", "O'Brian"));
-//	    repository.save(new Customer("Kim", "Bauer"));
-//	    repository.save(new Customer("David", "Palmer"));
-//	    repository.save(new Customer("Michelle", "Dessler"));
+	    // Get all customers from CUSTOMER table.
+	    var allCustomers = new ArrayList<>();
+	    repository.findAll().forEach(allCustomers::add);
 
-	    // fetch all customers
+	    // Populate CUSTOMER table if empty.
+	    if (allCustomers.isEmpty()) {
+		repository.save(new Customer("Jack", "Bauer"));
+		repository.save(new Customer("Chloe", "O'Brian"));
+		repository.save(new Customer("Kim", "Bauer"));
+		repository.save(new Customer("David", "Palmer"));
+		repository.save(new Customer("Michelle", "Dessler"));
+	    }
+
+	    // Re-populate allCustomers.
+	    allCustomers = new ArrayList<>();
+	    repository.findAll().forEach(allCustomers::add);
+
+	    // Log customers.
 	    log.info("Customers found with findAll():");
 	    log.info("-------------------------------");
-	    repository.findAll().forEach(customer -> {
+	    allCustomers.forEach(customer -> {
 		log.info(customer.toString());
 	    });
 	    log.info("");
 
-	    // fetch an individual customer by ID
+	    // Fetch an individual customer by ID.
 	    Customer customer = repository.findById(1L);
 	    log.info("Customer found with findById(1L):");
 	    log.info("--------------------------------");
-	    log.info(customer.toString());
+	    log.info(customer != null ? customer.toString() : "not found");
 	    log.info("");
 
 	    // fetch customers by last name
 	    log.info("Customer found with findByLastName('Bauer'):");
 	    log.info("--------------------------------------------");
 	    repository.findByLastName("Bauer").forEach(bauer -> {
-		log.info(bauer.toString());
+		log.info(bauer != null ? bauer.toString() : "not found");
 	    });
 	    log.info("");
 	};
